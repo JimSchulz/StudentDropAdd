cff6e4952ed99a5d62cea0e21dea848e06825e2c
Jim Schulz <jschulz@coloradocollege.edu>
2017-08-25 14:40:52 -0600

    New Policy Update

--- DropGrid-onClick.js
+++ DropGrid-onClick.js
@@ -8,7 +8,7 @@ for (i=0; i<table.rows.length-1; i++) {
 
   trk = document.getElementById("pbid-Track-" + i);
 
-  if (trk.options[trk.selectedIndex].text == "Drop") {
+  if (trk.disabled) {
     // Track select list is disabled because we're in the middle of a drop/add transaction.
     alert("You have chosen to drop a course.  You need to also add a course in the same block.  Please choose a new course.<br><br>To cancel this transaction, press the Cancel button.");
     break;
--- Track-onUpdate.js
+++ Track-onUpdate.js
@@ -4,50 +4,51 @@ var table = document.getElementById("pbid-DropGrid");
 var trk;
 var i;
 var j;
-drop = 0;
+var dropPolicy = 0;
+var block = 0;
+var units = 0;
 
 // Loop through all DropGrid rows
 for (i=0; i<table.rows.length-1; i++) {
 
+  block = $DropGrid.$data[i].BLOCK;
+  units = $DropGrid.$data[i].UNITS;
+
   trk = document.getElementById("pbid-Track-" + i);
 
-  if (trk.options[trk.selectedIndex].text == "Drop" && $DropGrid.$data[i].SCHEDULETYPE != '2') {
-    // Course is being dropped, and it's not an Adjunct course.
-    for (j=0; j<table.rows.length-1; j++) {
-      trk = document.getElementById("pbid-Track-" + j);
-      trk.disabled = true;  // Disable all Track selects
-    }
+  if (trk.options[trk.selectedIndex].text == "Drop") {
 
-    if ($DropGrid.$data[i].BLOCK == "1-2" || $DropGrid.$data[i].BLOCK == "2-3" || $DropGrid.$data[i].BLOCK == "3-4" || $DropGrid.$data[i].BLOCK == "5-6" || $DropGrid.$data[i].BLOCK == "6-7" || $DropGrid.$data[i].BLOCK == "7-8") {
+    // Course is being dropped
+
+    if ((block == "1-2" || block == "2-3" || block == "3-4" || block == "5-6" || block == "6-7" || block == "7-8") && (units == " 2.00")) {
       // Dropping a two-block course
+      disableTrackSelect();
       alert("You have chosen to drop a two-block course.  You need to also add a two-block course in the same blocks.  Please choose a new two-block course. If you wish to add two one-block courses, please see the registrars office.");
+      break;
     }
-    else if ($DropGrid.$data[i].BLOCK.length == 1) {
+    else if (block.length == 1 && units == " 1.00") {
+      // Dropping a one-block course
+      disableTrackSelect();
       alert("You have chosen to drop a one-block course.  You need to also add a one-block course in the same block.  Please choose a new course.");
+      break;
     }
-    else {
-      alert("This application does not support the dropping of blocks " + $DropGrid.$data[i].BLOCK + ". Please see the Registrars Office.");
-      return;
-    }
-
-    //$DropGrid.$load({clearCache:true});
-    //trk.value = "Drop";
-
-    document.getElementById("pbid-DropTerm").value = $DropGrid.$data[i].ACADPERIOD;  // Used in SearchClassSchedule-onClick
-    document.getElementById("pbid-DropBlock").value = $DropGrid.$data[i].BLOCK;      // Used in SearchClassSchedule-onClick
-    drop = 1;
-    break;
   }
 }
 
 $DropCourseButtonsForm.$visible = true;  // Show the Buttons form
 
-if (drop == 0) {
+if (dropPolicy == 0) {
+
+  // Grade Track change only
+
   // Show the DropGrid buttons (This is only a Grading Track change)
   document.getElementById('pbid-DropApplyChanges').style.display = 'block';
   document.getElementById('pbid-DropReset').style.display = 'block';
 }
 else {
+
+  // Course Drop
+
   // Hide the DropGrid Apply Changes button (This is a course drop)
   document.getElementById('pbid-DropApplyChanges').style.display = 'none';
   document.getElementById('pbid-DropReset').style.display = 'block';
@@ -58,6 +59,17 @@ else {
   // Reset the ClassSearch form
   document.getElementById('pbid-ClassSearch').reset();
 
-  // Hide BlockClassSearch
+  // Hide BlockClassSearch and BlockAddCourse
   $BlockClassSearch.$visible = false;
+  $BlockAddCourse.$visible = false;
+}
+
+function disableTrackSelect() {
+  for (j=0; j<table.rows.length-1; j++) {
+    trk = document.getElementById("pbid-Track-" + j);
+    trk.disabled = true;  // Disable All Track select objects
+  }
+  dropPolicy = 1;
+  document.getElementById("pbid-DropTerm").value = $DropGrid.$data[i].ACADPERIOD;  // Used in SearchClassSchedule-onClick
+  document.getElementById("pbid-DropBlock").value = $DropGrid.$data[i].BLOCK;      // Used in SearchClassSchedule-onClick
 }
