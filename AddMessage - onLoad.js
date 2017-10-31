5d05ce5fda72c210f3ecc83eff622d72c9981834
Jim Schulz <jschulz@coloradocollege.edu>
2017-10-31 14:49:34 -0600

    AddMessage update

--- AddMessage - onLoad.js
+++ AddMessage - onLoad.js
@@ -9,6 +9,6 @@ else {
   $AddEntryMessageForm.$visible = true;
 
   if ($AddMessage.ADD_MESSAGE == 'ADD FAILED: Registration status code WL is not type R.') {
-     $AddMessage.ADD_MESSAGE = 'ADD FAILED: Please select a course with open seats.';
+     $AddMessage.ADD_MESSAGE = "ADD FAILED: Please select a course with open seats. To be added to a course's wait list, please see the Registrar's Office.";
   }
 }
